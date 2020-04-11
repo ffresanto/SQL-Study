b3d04650f57205ac706e613803aa8b8dafe90ff3
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2020-04-11 13:33:02 -0300

    uasndo Having e SUM

--- operadores logicos.sql
+++ operadores logicos.sql
@@ -65,7 +65,7 @@ SELECT * FROM senso
 WHERE nome_mun LIKE '_a%'
 AND ano = '2014';
 
--- operador not
+-- operador NOT
 
 SELECT ano, cod_uf, estado, nome_mun, populacao FROM senso
 WHERE nome_mun NOT LIKE ('A%')
@@ -73,3 +73,62 @@ AND cod_uf = '35'
 AND NOT populacao > 40000
 AND ano = '2014';
 
+-- operador OR
+
+SELECT * FROM senso
+WHERE nome_mun LIKE ('a%')
+AND (cod_uf = '35' OR cod_uf = '15');
+
+-- evidencia de erro OR
+
+SELECT * FROM senso
+WHERE nome_mun LIKE ('a%')
+AND cod_uf = '35'
+AND cod_uf = '15';
+
+-- operador IS NOT NULL / retorna valores que não estao nulos
+
+SELECT * FROM senso
+WHERE regiao IS NOT NULL;
+
+-- operador IS NULL / retorn valores nulos
+
+SELECT * FROM senso
+WHERE regiao IS NULL;
+
+-- preparando cenario is null
+SELECT * FROM senso
+WHERE regiao = ''
+
+UPDATE senso SET regiao = NULL
+WHERE regiao = '';
+
+UPDATE senso SET regiao = ''
+WHERE regiao IS NULL;
+
+-- operador HAVING / COUNT
+
+SELECT cod_uf, estado, COUNT(*) qtd FROM senso
+WHERE ano = '2014'
+GROUP BY cod_uf, estado HAVING COUNT(cod_mun) > 500; 
+
+-- operador HAVING
+
+SELECT cod_uf, estado, COUNT(*) qtd FROM senso
+WHERE ano = '2014'
+GROUP BY cod_uf, estado HAVING COUNT(cod_mun) < 500; 
+
+-- operador HAVING / SUM
+
+SELECT cod_uf, estado, COUNT(cod_mun) AS qtd, SUM(populacao) AS soma_populacao FROM senso
+WHERE ano = '2014'
+GROUP BY cod_uf, estado HAVING SUM(populacao) > 5000000;
+
+-- operador HAVING 
+
+SELECT cod_uf, estado, COUNT(cod_mun) AS qtd, SUM(populacao) AS soma_populacao FROM senso
+WHERE ano = '2014'
+GROUP BY cod_uf, estado HAVING SUM(populacao) < 5000000;
+
+SELECT ano, SUM(populacao) FROM senso
+GROUP BY ano
